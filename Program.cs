2a545bd6bc9d68cea7557a764bdc6610e79946f9
Jade <jade.nguyen@zoopla.co.uk>
2020-07-15 13:49:44 +0100

    added files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace Bus_Board
 {
@@ -6,7 +8,13 @@ namespace Bus_Board
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var client = new RestClient("https://transportapi.com/v3/uk/bus/stop/490008660N/live.json?app_id=171db06a&app_key=ce4314169d53cd484b70d421f63d27d7&group=route&nextbuses=yes");
+            client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+
+            var timeline = await client.GetAsync<ApiResponse>(request, cancellationToken);
         }
     }
 }
---
+++ TransportApiResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Bus_Board
+{
+    public class ApiResponse {
+    public string FirstThing { get; set; }
+    public List<string> MyList { get; set; }
+         
+    }
+}
